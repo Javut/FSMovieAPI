3a8c7ba33a452b493601f28d05c1ef71b3b3c26a
jaider <javut2010@hotmail.com>
2024-03-18 10:04:34 -0500

    first commit

---
+++ src/test/java/dev/javut/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javut.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
